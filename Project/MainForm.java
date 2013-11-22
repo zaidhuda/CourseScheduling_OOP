013ac83107cd70af09010824f234dca2e343d233
Zaid Huda <akuzaid93@yahoo.com>
2013-11-23 06:43:16 +0800

    Sample Main Form
    Sample Main Form source code

--- Project/MainForm.java
+++ Project/MainForm.java
@@ -3,23 +3,16 @@ import java.awt.*;
 import java.awt.event.*;
 import java.util.ArrayList;
 
-public class CourseForm{
+public class MainForm{
 	public static ArrayList<Course> courses = new ArrayList<Course>();
 	Course course = new Course((String) null, (String) null, 0);
-	JFrame frame = new JFrame("CourseForm");
+	JFrame frame = new JFrame("MainForm");
 	JPanel panel = new JPanel();
 	JButton btnOK = new JButton("OK");
-	final JTextField code = new JTextField(25);
-	final JTextField title = new JTextField(25);
-	final JTextField credit = new JTextField(25);
 	ButtonListener btnListener = new ButtonListener(); 
 
-	public CourseForm(ArrayList<Course> courses){
+	public MainForm(ArrayList<Course> courses){
 		this.courses = courses;
-
-		panel.add(code);
-		panel.add(title);
-		panel.add(credit);
 		panel.add(btnOK);
 
 		panel.setLayout(new GridLayout(4,1));
@@ -35,11 +28,7 @@ public class CourseForm{
 	private class ButtonListener implements ActionListener{
 		public void actionPerformed (ActionEvent event){
 			try{
-				course.setCode(code.getText());
-				course.setTitle(title.getText());
-				course.setCredit(Integer.parseInt(credit.getText()));
-				courses.add(course);
-				JOptionPane.showMessageDialog(null, course);
+				JOptionPane.showMessageDialog(null, courses);
 			}
 			catch(Exception e){
 				JOptionPane.showMessageDialog(null, e);
